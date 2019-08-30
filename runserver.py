56b14dbbca07838c3a27cf7e0b77fa4755516db3
Alan Ji <abj3@rice.edu>
2019-08-29 23:07:02 -0500

    Initial commit.

---
+++ runserver.py
@@ -0,0 +1,69 @@
+"""
+This script runs the ImageSimilarity application using a development server.
+"""
+
+import os, shutil
+from similar import findsim
+from flask import Flask, render_template, request, session, url_for, redirect
+from flask_dropzone import Dropzone
+from flask_uploads import UploadSet, configure_uploads, patch_request_class, IMAGES
+import pathlib
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    os.mkdir("static/uploads")
+except:
+    pass
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "key"
+app.config.update(
+    UPLOADED_PATH=os.path.join('static', 'uploads'),
+    DROPZONE_ALLOWED_FILE_TYPE='image',
+    DROPZONE_MAX_FILE_SIZE=3,
+    #DROPZONE_MAX_FILES=100,
+    DROPZONE_PARALLEL_UPLOADS=100,
+    DROPZONE_UPLOAD_MULTIPLE=True,
+)
+
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
+
+dropzone = Dropzone(app)
+
+@app.route('/', methods=['POST', 'GET'])
+def upload():
+    if "file_urls" not in session:
+        session['file_urls'] = []
+    filenames = session['file_urls']
+    if request.method == 'POST':
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                path = os.path.join(app.config['UPLOADED_PATH'], f.filename)
+                f.save(path)
+                filenames.append(path)
+        session['file_urls'] = filenames
+    return render_template('index.html')
+
+@app.route('/query', methods=['POST', 'GET'])
+def query():
+    if "file_urls" not in session or session['file_urls'] == []:
+        return redirect(url_for('upload'))
+    filenames = session['file_urls']
+    print ("hi" , filenames)
+    topn = findsim(filenames)
+    sim = []
+    for i in topn[0]:
+        print (i)
+        sim.append(os.path.join(app.config['UPLOAD_FOLDER'], os.path.split(filenames[i])[1]))
+    session.pop('file_urls', None)
+    return render_template('results.html', file_urls = sim)
+
+if __name__ == '__main__':
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug = True)
---
+++ similar.py
@@ -0,0 +1,97 @@
+from numpy import *
+from PIL import Image
+import time
+import os
+import scipy.spatial.distance as dis
+
+bins = 4
+rv = 256
+
+def signature(img, sec):
+    im = img.convert('RGB')
+    W,H = im.size
+    colors = im.getcolors(W*H)
+
+    d = {}
+    for count, color in colors:
+       k = [int(x / (256 / sec)) for x in color]
+       k = str(k)
+       if k not in d:
+           d[k] = []
+       d[k].append(count)
+
+    hashes = []
+    for r in range(0, sec):
+        for g in range(0, sec):
+            for b in range(0, sec):
+                k = str([r, g, b])
+                v = sum(d[k]) if k in d else 0
+                hashes.append(v)
+    return hashes
+
+def sketch(M, k):
+    w,h = M.shape
+
+
+    rand = random.randn(k, h)
+
+    sketches = zeros((k,w))
+
+    for i in range(k):
+        for j in range(w):
+            v = dot(rand[i], M[j])
+            if v > 0:
+                sketch = 1
+            else:
+                sketch = 0
+            sketches[i][j] = sketch
+    return sketches
+
+def cossim(s1, s2):
+    result = dot(s1, s2) / dot(linalg.norm(s1), linalg.norm(s2))
+
+    if allclose(result, 1):
+        ac = 0
+    else:
+        ac = arccos(result)
+    return 1 - ac
+
+def loadImages(path):
+    imagesList = os.listdir(path)
+    loadedImages = []
+    for image in imagesList:
+        img = Image.open(path + image)
+        loadedImages.append(img)
+    return loadedImages
+
+def findsim(filenames):
+    imgs = []
+    for filename in filenames:
+        img = Image.open(filename)
+        imgs.append(img)
+    mat = zeros(int(math.pow(bins, 3)), dtype = int)
+
+    for img in imgs:
+        sigs = signature(img, bins)
+        mat = vstack((sigs, mat))
+
+    mat = mat[:-1]
+
+    sketches = sketch(mat, rv)
+
+    pic_sketch = sketch(array([signature(Image.open("data/anchor/image_0005.jpg"), bins)]), rv)
+
+    scores = dot(transpose(pic_sketch), sketches)
+    print (scores)
+    n = 10
+    top_n = argsort(scores)[-n:][::-1]
+    return top_n
+
+
+
+
+
+
+
+
+
